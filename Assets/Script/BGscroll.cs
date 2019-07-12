757da4658d9172a7bbec18c4316855bfd3984e38
LissetteHuilca <lissette.huilca@gmail.com>
2019-07-12 13:27:01 -0500

    AddScript

--- Assets/Script/BGscroll.cs
+++ Assets/Script/BGscroll.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class BGscroll : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float scroll_Speed = 0.1f;
+
+    private MeshRenderer mesh_Renderer;
+
+    private Vector2 saved_Offset;
+    
+    void Awake()
     {
-        
+        mesh_Renderer = GetComponent<MeshRenderer>();
+
+        saved_Offset = mesh_Renderer.sharedMaterial.GetTextureOffset("_MainTex");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float y = Time.time * scroll_Speed;
+
+        Vector2 offset = new Vector2(0, y);
+
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+
+    }
+
+    void OnDisable()
+    {
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", saved_Offset);
     }
 }
