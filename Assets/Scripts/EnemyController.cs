ffd6fd483ed8aa849281a4bfacd0406b7a35fdd3
Xavier <xav_andrade@hotmail.com>
2019-07-13 14:57:37 -0500

    WeaponController (RayCast shooting) plus DemoScene, missing animation

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public int health = 100;
+
+    void FixedUpdate()
+    {
+        if (this.health <= 0)
+            Die();
+    }
+
+    // Update is called once per frame
+    public int LowerHealth(int damage)
+    {
+        this.health -= damage;
+        Debug.Log($"OUCH! I'm hit! {this.health}");
+        return this.health;
+    }
+
+    void Die()
+    {
+        // TODO Play animation here
+        // Remove object
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public Transform weapon;
+
+  /*   public LayerMask whatToHit;
+    public float range = 100f;
+    public float hitForce = 400f; */
+
+    public int damageRatio = 25;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireWeapon();
+        }
+    }
+
+    void FireWeapon()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(weapon.position, weapon.up);
+        //Debug to check ray is created and firing
+        Debug.DrawRay(weapon.position, weapon.up);
+
+        EnemyController enemy;
+        if (hitInfo)
+        {
+            if ((enemy = hitInfo.transform.GetComponent<EnemyController>()) != null)
+            {
+                //Make Enemy Take Damage and other stuff
+                enemy.LowerHealth(damageRatio);
+            }
+        }
+    }
+
+
+}
