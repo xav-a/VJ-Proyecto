1305348a5624a2150d25eff8279e92e2712a747e
Luis Eduardo Salazar Castro <luis8e@hotmail.com>
2019-07-21 15:35:01 -0500

    The obstacle generator was created and the elements are removed when they pass the camera limits

--- Assets/Script/Obstaculos.cs
+++ Assets/Script/Obstaculos.cs
@@ -6,19 +6,34 @@ public class Obstaculos : MonoBehaviour
 {
 
     public float velocidad =100f;
+    private Transform trans;
     public Rigidbody2D rb;
+    private GameObject thisObs;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = Vector2.up * -velocidad + Vector2.right * Random.Range(-50,50);
         rb.angularVelocity = 80f;
+        trans = GetComponent<Transform>();
+        thisObs = trans.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float posx = trans.position.x;
+        float posy = trans.position.y;
+        //-19
+        if (posy<-19 && thisObs!=null)
+        {
+         //   Debug.Log("se paso de y");
+            Destroy(thisObs);
+        }else if ((posx<34 || posx>804) && thisObs != null)
+        {
+            Destroy(thisObs);
+        }
+
     }
 
    private void OnCollisionEnter2D(Collision2D col)
@@ -33,8 +48,6 @@ public class Obstaculos : MonoBehaviour
             GameObject.Destroy(HplayerGO);
             GameObject.Destroy(obsGO);
         }
-
-
     }
 
 }
